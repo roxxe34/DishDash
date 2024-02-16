fc79e6d734e63644e9b1fd5bc1a339449241ba77
Hamza Farissi <hamzafarissi7@gmail.com>
2024-02-16 01:18:06 +0100

    d

--- djangoProject/dishdash/views.py
+++ djangoProject/dishdash/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+
 
 
 # Create your views here.
 def salam(request):
-    return HttpResponse('salaaaaaaam')
+    mydict = {'insert_me': "Hello I am from views.py!"}
+    return render(request, 'first_app/index.html')
--- djangoProject/djangoProject/urls.py
+++ djangoProject/djangoProject/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from dishdash import views
+from django.urls import include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.salam)
 ]
